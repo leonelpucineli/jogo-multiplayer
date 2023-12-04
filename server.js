92aa66acd0f0db44e3b30025566ca3ba95668cbf
leonel <leonelpucineli@gmail.com>
2023-12-04 20:15:37 -0300

    Initial commit (2)

---
+++ server.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import http from 'http';
+import createGame from './public/game.js';
+//import socketio from 'socket.io';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+//const sockets = socketio(server);
+const sockets = new Server(server, {
+    serveClient: true,
+});
+  
+
+app.use(express.static('public'));
+
+const game = createGame();
+game.start();
+
+game.subscribe((command) => {
+    console.log(`> Emmitting ${command.type}`);
+    sockets.emit(command.type, command);
+});
+
+console.log(game.state);
+
+sockets.on('connection', (socket) => {
+    const playerId = socket.id;
+    console.log(`Player connected on Server with id: ${playerId}`);
+    console.log(game.state)
+
+    game.addPlayer({ playerId: playerId });
+
+    socket.emit('setup', game.state);
+
+    socket.on('disconnect', () => {
+        game.removePlayer({ playerId: playerId});
+        console.log(`> Player disconnected: ${playerId}`);
+    });
+
+    socket.on('move-player', (command) => {
+        command.playerId = playerId;
+        command.type = 'move-player';
+
+        game.movePlayer(command);
+    });
+});
+
+server.listen(3000, () => {
+    console.log(`> Server listening on port: 3000`);
+});
